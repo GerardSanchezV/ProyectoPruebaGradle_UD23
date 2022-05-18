13a052624bae3a4e19f8198db43a2bc247eb9585
GerardSanchezV <gerardsanchezvelasco@gmail.com>
2022-05-18 19:19:59 +0200

    Initial commit

---
+++ src/main/java/UD24/Spring/ProyectoPruebaGradleApplication.java
@@ -0,0 +1,13 @@
+package UD24.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPruebaGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPruebaGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UD24/Spring/ProyectoPruebaGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package UD24.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPruebaGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
